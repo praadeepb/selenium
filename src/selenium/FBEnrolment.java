72dd585f2b77958b2fb0ec96634c291d667fa08b
Balasubramanian, Praadeep (ITCDEC) - KLM <Praadeep.Balasubramanian@klm.com>
2019-06-12 17:32:25 +0200

    First project created

---
+++ src/selenium/FBEnrolment.java
@@ -0,0 +1,8 @@
+package selenium;
+
+public class FBEnrolment {
+public static void main(String[] args) {
+	Validations V = new Validations();
+	V.validateHeader("Home page");
+}
+}
---
+++ src/selenium/Validations.java
@@ -0,0 +1,8 @@
+package selenium;
+
+public class Validations {
+	public boolean validateHeader(String page) {
+		System.out.println(page+" header validated");
+	return true;	
+	}
+}
